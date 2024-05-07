bb3306c708b372c9864eda6587d674c05b390eb9
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2024-05-06 23:39:49 -0300

    Add files via upload

---
+++ assets/js/exApp.js
@@ -0,0 +1,43 @@
+function exercicioDoRelogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pr-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+    
+    const relogio = document.querySelector('.relogio');
+    const iniciar = document.querySelector('.iniciar');
+    const pausar = document.querySelector('.pausar');
+    const zerar = document.querySelector('.zerar');
+    let segundos = 0;
+    let timer;
+    
+    function iniciarRelogio() {
+        timer = setInterval(function() {
+            segundos++
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    }
+    
+    iniciar.addEventListener('click', function(event) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciarRelogio();
+    });
+    
+    pausar.addEventListener('click', function(event) {
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+    });
+    
+    zerar.addEventListener('click', function(event) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    });
+}
+
+exercicioDoRelogio();
